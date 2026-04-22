cc53b4ebe527d2f09654fecb730f530e041b868a
alexxanton <alexantonaranda@gmail.com>
2026-04-23 01:39:10 +0200

    updated error handling

--- src/parser/transformer.py
+++ src/parser/transformer.py
@@ -4,16 +4,21 @@ from typing import List, Dict, Tuple, Set, Any, Union
 
 class DroneTransformer(Transformer):
     def __init__(self) -> None:
+        self._nb_drones: int = 0
         self._hub_names: Set[str] = set()
         self._connections: Set[Tuple[str, str]] = set()
         self._valid_zones: Set[str] = {
             "normal", "blocked", "restricted", "priority"
+        }
+        self._valid_hub_meta: Set[str] = {
+            "zone", "color", "max_drones"
         }
 
     def nb_drones(self, args: List[Any]) -> Dict[str, int]:
         val: int = int(args[0])
         if val < 0:
             raise ValueError("nb_drones must be a positive integer")
+        self._nb_drones = val
         return {"nb_drones": val}
 
     def name_coord(self, args: List[Any]) -> Tuple[str, int, int]:
@@ -24,7 +29,25 @@ class DroneTransformer(Transformer):
         return name, x, y
 
     def hub_line(self, args: List[Any]) -> Dict[str, Any]:
-        return {"type": str(args[0]), "params": args[1], "metadata": args[2]}
+        hub_type = str(args[0])
+        meta = args[2]
+
+        for key in meta.keys():
+            if key not in self._valid_hub_meta:
+                raise ValueError(f"Unknown key {key}")
+
+        if "zone" in meta and meta["zone"] not in self._valid_zones:
+            raise ValueError(f"Unknown zone {meta['zone']}")
+
+        if "start" in hub_type or "end" in hub_type:
+            if not "max_drones" in meta:
+                meta["max_drones"] = self._nb_drones
+            elif meta["max_drones"] != self._nb_drones:
+                raise ValueError(
+                    "{hub_type}: Max drones quantity doesn't match"
+                )
+
+        return {"type": hub_type, "params": args[1], "metadata": meta}
 
     def pair(self, args: List[Any]) -> Tuple[str, Union[int, str]]:
         return (
