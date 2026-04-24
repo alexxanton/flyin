340e11ec70b4116440e765803067821bc6fad536
alexxanton <alexantonaranda@gmail.com>
2026-04-24 18:03:57 +0200

    added different speed values

--- src/entity/drone.py
+++ src/entity/drone.py
@@ -6,16 +6,18 @@ class Drone(Entity):
     def __init__(self, x: int, y: int, hub: Hub) -> None:
         super().__init__(x, y)
         self._hub: Hub = hub
-        self._progress: int = 0
+        self._progress: float = 0
         self._hub.land_on()
         self._og_x = x
         self._og_y = y
         self._next_x = x
         self._next_y = y
+        self._speed: int = 0
 
     def _fly_to_hub(self, next_hub: Hub) -> None:
         self._og_x, self._og_y = self._hub.pos
         self._next_x, self._next_y = next_hub.pos
+        self._speed = 1 if next_hub.zone == "restricted" else 2
         self._hub.take_off()
         self._hub = next_hub
         next_hub.land_on()
@@ -23,7 +25,7 @@ class Drone(Entity):
 
     def update(self) -> None:
         if self._progress > 0:
-            self._progress += 2
+            self._progress += self._speed * 0.7
             x = (self._next_x - self._og_x) * self._progress / 100 + self._og_x
             y = (self._next_y - self._og_y) * self._progress / 100 + self._og_y
             self.pos = (x, y)
